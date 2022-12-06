35b96a4f324c85810b430b4b8a0bcb95fde34559
Paul Song <jaebinpsong@gmail.com>
2022-12-06 07:03:28 -0800

    Added try and catch for double points in operation and cleaned code

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let firstOperand = ''
-let secondOperand = ''
-let currentNumber = ''
 let operatorPressed = false;
-let operatorUsed = ''
+
 
 const numberBtn = document.querySelectorAll("[data-number]")
 const operatorBtn = document.querySelectorAll("[data-operator]")
@@ -11,40 +8,74 @@ const deleteBtn = document.querySelector("#delete")
 const equalBtn = document.querySelector('#equalsBtn')
 const pointBtn = document.querySelector('#pointBtn')
 
-let topSreen = document.querySelector('#topscreen')
+let topScreen = document.querySelector('#topscreen')
 let bottomScreen = document.querySelector('#bottomscreen')
 
 const inputNum = (button) => {
     let num = button.textContent
-    if(bottomScreen.textContent == '' && num == 0){
-
-    }else {
+    if(!(bottomScreen.textContent == '' && num == 0)){
         bottomScreen.textContent += num
+        //makes possible for more operations in one line
         operatorPressed = false
     }
     
 }
 
-
-operatorBtn.forEach(button => {
-    button.addEventListener('click', function() {
-        if(bottomScreen.textContent == ''){
-
-        }
-        //so two operators are not next to each other
-        else if(operatorPressed && secondOperand == ''){
-            let str = bottomScreen.textContent.slice(0,-1) + button.textContent
-            bottomScreen.textContent = str
-        }
-        else {
-            bottomScreen.textContent += button.textContent
-            operatorPressed = true
-            operatorUsed = button.textContent
-        }
-        
+const addOperator = () => {
+    //so two operators are not next to each other
+    if(operatorPressed){
+        let str = bottomScreen.textContent.slice(0,-1) + button.textContent
+        bottomScreen.textContent = str
     }
-    )
-})
+    else {
+        bottomScreen.textContent += button.textContent
+        operatorPressed = true
+        operatorUsed = button.textContent
+    }
+}
+
+const disable = () => {
+    const btn = document.querySelectorAll('.btn')
+    btn.forEach(btn => {
+        if(btn.textContent != 'Clear'){
+            btn.classList.add('disable')
+        }
+    })
+}
+
+const enable = () => {
+    const btn = document.querySelectorAll('.btn')
+    btn.forEach(btn => {
+        btn.classList.remove('disable')
+    })
+}
+
+const equal = () => {
+    try {
+        eval(bottomScreen.textContent)
+    } catch (error){
+        bottomScreen.textContent = "ERROR CLEAR AND TRY AGAIN"
+        disable()
+    }
+
+    let str = bottomScreen.textContent.slice(-1)
+    if(!!str.match(/[^0-9]/)){
+        topScreen.textContent = ''
+        bottomScreen.textContent = 'ERROR CLEAR AND TRY AGAIN'
+        disable()
+    } else if(eval(bottomScreen.textContent) == 'Infinity'){
+        topScreen.textContent = ''
+        bottomScreen.textContent = 'ERROR CLEAR AND TRY AGAIN'
+        disable()
+    } else if(Number.isInteger(eval(bottomScreen.textContent))){
+        bottomScreen.textContent = eval(bottomScreen.textContent)
+        topScreen.textContent = bottomScreen.textContent
+    }
+    else{
+        bottomScreen.textContent = parseFloat(eval(bottomScreen.textContent).toFixed(3))
+        topScreen.textContent = bottomScreen.textContent
+    }
+}
 
 numberBtn.forEach(button => {
     button.addEventListener('click', function() {
@@ -52,20 +83,28 @@ numberBtn.forEach(button => {
     })
 })
 
-equalBtn.addEventListener('click', function() {
-    let str = bottomScreen.textContent.slice(-1)
-    if(!!str.match(/[^0-9]/)){
-        bottomScreen.textContent = 'ERROR CLEAR AND TRY AGAIN'
-    } else {
-        bottomScreen.textContent = eval(bottomScreen.textContent)
-        topSreen.textContent = bottomScreen.textContent
+operatorBtn.forEach(button => {
+    button.addEventListener('click', function() {
+        addOperator()
     }
+    )
+})
+
+equalBtn.addEventListener('click', function() {
+    //catches for any error ex. double points
+    equal()
 })
 
 clearBtn.addEventListener('click',() => {
     bottomScreen.textContent = ''
+    topScreen.textContent = ''
+    enable()
 })
 
 deleteBtn.addEventListener('click',() => {
     bottomScreen.textContent = bottomScreen.textContent.slice(0,-1)
+})
+
+pointBtn.addEventListener('click',() => {
+    bottomScreen.textContent += '.'
 })
